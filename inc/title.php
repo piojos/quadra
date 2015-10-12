895878ca07c839af4f265e57959b059589320420
raidho-mx <miranda@raidho.mx>
2015-10-12 16:37:44 -0500

    Office & inc/title

--- inc/title.php
+++ inc/title.php
@@ -1,4 +1,4 @@
-<div class="title" style="background-image: url('http://lorempixel.com/1400/800');">
+<div class="title" style="background-image: url('img/render1.jpg');">
 	<div>
 		<p>DISEÑO POR VIDAL ARQUITECTOS</p>
 	</div>
