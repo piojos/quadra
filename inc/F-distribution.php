526a4d001679d93a0c92d41a3c9d41de3b69aed0
raidho-mx <miranda@raidho.mx>
2015-10-15 17:07:50 -0500

    Cleaner Structure

---
+++ inc/F-distribution.php
@@ -0,0 +1,10 @@
+<div style="
+	width:100%;
+	text-align:center;
+	padding:2em;
+">
+
+	<h1>Distribution</h1>
+	<p>This is definitely missing.</p>
+
+</div>
--- page.php
+++ page.php
@@ -1,3 +1,43 @@
-<?php get_header(); ?>
+<?php
 
-<?php get_footer(); ?>
+get_header();
+
+
+while(have_rows('')): the_row();
+
+	if(get_row_layout() == 'img_slider'):
+
+		get_template_part('inc/A', 'slider');
+
+
+	elseif(get_row_layout() == 'info_slider'):
+
+		get_template_part('inc/B', 'slider_quotes');
+
+
+	elseif(get_row_layout() == 'halfs'):
+
+		get_template_part('inc/C', 'halfs');
+
+
+	elseif(get_row_layout() == 'title_bg'):
+
+		get_template_part('inc/D', 'title');
+
+
+	elseif(get_row_layout() == 'contact'):
+
+		get_template_part('inc/E', 'contact');
+
+
+	elseif(get_row_layout() == 'distribution'):
+
+		get_template_part('inc/F', 'distribution');
+
+
+	endif;
+
+endwhile;
+
+
+get_footer(); ?>
