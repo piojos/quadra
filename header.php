77f94f3933adf2a17e668d021ba84374d269efc8
Linda Castillor <linda@raidho.mx>
2015-10-26 10:35:31 -0600

    Remove script for slider with arrows

--- header.php
+++ header.php
@@ -63,36 +63,6 @@
 			openEffect    : 'none',
 			closeEffect    : 'none'
 		});
-		// var unslider = $('.slider_arrows').unslider();
-		//
-		// $('.unslider-arrow').click(function() {
-		// 	var fn = this.className.split(' ')[1];
-		//
-		// 	unslider.data('unslider')[fn]();
-		// });
-
-
-		$(function() {
-			// $('.banner').unslider({
-			// 	speed: 500,               //  The speed to animate each slide (in milliseconds)
-			// 	delay: 3000,              //  The delay between slide animations (in milliseconds)
-			// 	complete: function() {},  //  A function that gets called after every slide animation
-			// 	keys: true,               //  Enable keyboard (left, right) arrow shortcuts
-			// 	dots: true,               //  Display dot navigation
-			// 	fluid: false              //  Support responsive design. May break non-responsive designs
-			// });
-
-			var unslider = $('.slider_arrows').unslider();
-
-			// $('.unslider-arrow').click(function() {
-			// 	unslider.data('unslider').prev();
-			// });
-
-			$('.unslider-arrow').click(function() {
-				unslider.data('unslider').next();
-			});
-			return false;
-		});
 	</script>
 
 	<!-- Analytics -->
